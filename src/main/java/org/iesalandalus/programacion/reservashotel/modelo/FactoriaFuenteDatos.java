88ba92dc13c212703d492ca6e9f050a86ede4209
Jose Javier Sierra Berdun <jsieber1908@gmail.com>
2024-06-11 17:00:34 +0200

    Fuente de datos MySQL implementada

--- src/main/java/org/iesalandalus/programacion/reservashotel/modelo/FactoriaFuenteDatos.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/modelo/FactoriaFuenteDatos.java
@@ -4,6 +4,7 @@ import org.iesalandalus.programacion.reservashotel.modelo.negocio.IFuenteDatos;
 import org.iesalandalus.programacion.reservashotel.modelo.negocio.fichero.FuenteDatosFichero;
 import org.iesalandalus.programacion.reservashotel.modelo.negocio.memoria.FuenteDatosMemoria;
 import org.iesalandalus.programacion.reservashotel.modelo.negocio.mongodb.FuenteDatosMongoDB;
+import org.iesalandalus.programacion.reservashotel.modelo.negocio.mysql.FuenteDatosMySQL;
 
 public enum FactoriaFuenteDatos {
     MEMORIA{
@@ -18,6 +19,9 @@ public enum FactoriaFuenteDatos {
     },
     FICHERO{
         public IFuenteDatos crear() { return new FuenteDatosFichero(); }
+    },
+    MYSQL{
+        public IFuenteDatos crear() { return new FuenteDatosMySQL(); }
     };
     public abstract IFuenteDatos crear();
 }
